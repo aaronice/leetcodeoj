54c9332bf2ef572d398218e72a4d21ea2a6125e4
Aaron H. <aaronice@users.noreply.github.com>
2014-10-30 18:25:06 -0700

    Create ReverseInt.java

---
+++ ReverseInt.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public int reverse(int x) {
+        boolean nega=false;
+        if (x<0) {
+            nega = true;
+            x = -x;
+        }
+        long out = 0; 
+        while(x/10 != 0){
+            out = out*10 + x%10;
+            x = x/10;
+        }
+        out = out*10 + x%10;
+        
+        if(out>0x7FFFFFFF) return 0;
+        if(nega == true) return -(int)out;
+        return (int)out;
+    }
+}
