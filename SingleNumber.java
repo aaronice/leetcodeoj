fa5f2512059d5c069641dcfa268cf32ce73c931a
Aaron H. <aaronice@users.noreply.github.com>
2014-10-30 18:20:52 -0700

    Create SingleNumber.java

---
+++ SingleNumber.java
@@ -0,0 +1,18 @@
+
+public class SingleNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	class Solution {
+		public int singleNumber(int[] A) {
+			int result=0;
+			for(int i=0;i<A.length;i++){
+				result ^= A[i];
+			}
+			return result;
+		}
+	}
+}
