b1eda93d2c96e6e5e49a0e6eb3277aa22887b86b
zibing <tercelhuang@gmail.com>
2015-03-29 03:06:58 -0700

    03/29/2015

--- BestTimeStock.java
+++ BestTimeStock.java
@@ -1,13 +1,11 @@
 public class BestTimeStock {
-    class Solution {
-        public int maxProfit(int[] prices) {
-            int total = 0;
-            for (int i=0; i< prices.length-1; i++) {
-                if (prices[i+1]>prices[i]) total += prices[i+1]-prices[i];
-            }
 
-            return total;
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            if (prices[i + 1] > prices[i]) total += prices[i + 1] - prices[i];
         }
-    }
 
+        return total;
+    }
 }
